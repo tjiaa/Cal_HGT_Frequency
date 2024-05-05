b3c8fb8b020bbd5ed2145fa505648ccbd62e7b7c
tjiaa <91398760+tjiaa@users.noreply.github.com>
2024-05-05 11:42:28 +0800

    Delete HGT_Frequency/blastn.sh

--- HGT_Frequency/blastn.sh
+++
@@ -1,6 +0,0 @@
-route=/node05/program/shenzhen/round2/blastn/Motifi_Extract_Fasta
-
-for k in `ls ${route}`
-do
-blastn -query /node05/program/round2/blastn/Motifi_Extract_Fasta/${k} -db merged_DB/merged -outfmt ' 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send qseq sseq evalue bitscore' -out /node05/program/round2/blastn/OUT/${k}.txt
-done
