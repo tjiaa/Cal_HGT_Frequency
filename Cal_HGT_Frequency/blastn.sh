49cc93ffd49a1a01e1eebee391d54a8f810fc071
tjiaa <91398760+tjiaa@users.noreply.github.com>
2024-06-13 22:48:54 +0800

    Delete Cal_HGT_Frequency directory

--- Cal_HGT_Frequency/blastn.sh
+++
@@ -1,6 +0,0 @@
-route=/Users/jiachenghao/Desktop/shenzhen_test/blastn/Motifi_Extract_Fasta
-
-for k in `ls ${route}`
-do
-blastn -query /Users/jiachenghao/Desktop/shenzhen_test/blastn/Motifi_Extract_Fasta/${k} -db merged_DB/merged -outfmt ' 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send qseq sseq evalue bitscore' -out /Users/jiachenghao/Desktop/shenzhen_test/blastn/OUT/${k}.txt
-done
